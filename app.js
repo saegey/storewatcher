c0eb5a2ba74fa56834845ab9d47159b0d9f6d865
Adam Saegebarth <adams@hasoffers.com>
2015-06-14 17:58:00 -0700

    make work with heroku

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var _ = require('lodash');
 
+app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -113,8 +114,6 @@ app.post('/stores', checkApiKey, function (req, res) {
   });
 });
 
-var server = app.listen(process.env.PORT || 3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('App listening at http://%s:%s', host, port);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
